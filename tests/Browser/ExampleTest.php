213633f60710a9a9f10bb6849144a296c9e9681e
Nezar Fadle <nezarfadle@gmail.com>
2017-05-06 18:14:06 +0800

    adding user can compolete todo test

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -35,6 +35,7 @@ class ExampleTest extends DuskTestCase
 
     public function test_user_can_edit_todo()
     {
+       
         factory(User::class)->create( [ 'id' => 1, 'email' => 'user1@gmail.com' ] );
         $this->browse(function (Browser $browser) {
             
@@ -63,28 +64,26 @@ class ExampleTest extends DuskTestCase
      */
     public function test_user_can_filter_completed()
     {
+
         factory(User::class)->create( [ 'id' => 1, 'email' => 'user1@gmail.com' ] );
-        factory(Todo::class, 5)->create();
+        factory(Todo::class, 5)->create([ 'user_id' => 1]);
 
         $this->browse(function (Browser $browser) {
-            
      
             $this->assertDatabaseMissing( 'todos', [ 'id' => 1, 'complete' => true ] );
-            $this->assertDatabaseMissing( 'todos', [ 'id' => 2, 'complete' => true ] );
+            $this->assertDatabaseMissing( 'todos', [ 'id' => 5, 'complete' => true ] );
             
             $browser->loginAs(User::find(1))
                     ->visit('/')
                     ->waitFor('li.completed')
                     ->check('.todo-list > li:nth-child(1) .toggle')
                     ->check('.todo-list > li:nth-child(5) .toggle')
-                    ->pause(1000)
+                    ->pause(500)
             ;
 
             $this->assertDatabaseHas( 'todos', [ 'id' => 1, 'complete' => true ] );
-            $this->assertDatabaseHas( 'todos', [ 'id' => 2, 'complete' => true ] );
+            $this->assertDatabaseHas( 'todos', [ 'id' => 5, 'complete' => true ] );
             
-            
-
         });
     }
 }
